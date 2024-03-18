a6c7b740a38045d5abf4b08cd214eae75d087f0b
Kim <kimberley.quenon.@gmail.com>
2024-03-18 10:30:15 +0100

    first commit - bases

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div class="w-full h-screen bg-gray-500">1</div>
+    <div class="w-full h-screen bg-gray-700">2</div>
+    <div class="w-full h-screen bg-gray-900">3</div>
+    <BackToTop></BackToTop>
+  </div>
+</template>
---
+++ components/BackToTop.vue
@@ -0,0 +1,16 @@
+<template>
+    <button
+      class="fixed bottom-4 right-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      @click="scrollToTop">
+      Back to Top
+    </button>
+  </template>
+  
+  <script setup>
+    const scrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+    }
+  </script>
