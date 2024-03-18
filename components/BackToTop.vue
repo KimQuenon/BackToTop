d7ce7b02171ea6c33b8fe96a32bd3728eb17f167
Kim <kimberley.quenon.@gmail.com>
2024-03-18 14:10:16 +0100

    progression wrap

--- components/BackToTop.vue
+++ components/BackToTop.vue
@@ -1,41 +1,42 @@
 <template>
-  <div class="fixed top-0 left-0 w-full h-2 bg-gray-200">
-    <div
-      class="h-2 bg-blue-500"
-      :style="{ width: scrollPercentage + '%', transition: 'width 0.2s' }"
-    ></div>
+  <div class="fixed bottom-6 right-6">
+    <div class="flex align-center justify-center rounded-full px-1 py-1 w-full" ref="scrollProgress">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded-full"
+        @click="scrollToTop">
+        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"/>
+        </svg>
+      </button>
+    </div>
   </div>
-  <button
-    class="fixed bottom-6 right-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-3 rounded-full"
-    @click="scrollToTop">
-    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"/>
-    </svg>
-  </button>
 </template>
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 const scrollToTop = () => {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-  }
-  
-const scrollPercentage = ref(0);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+}
 
-const updateScrollPercentage = () => {
-  const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-  scrollPercentage.value = (scrollPosition / scrollHeight) * 100;
-};
+const scrollProgress = ref(null);
 
 onMounted(() => {
-  window.addEventListener('scroll', updateScrollPercentage);
+  let scrollPercentage = () => {
+      let pos = window.scrollY;
+      let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      let scrollValue = Math.round(pos * 100 / calcHeight);
+      console.log(scrollValue);
+
+      scrollProgress.value.style.background = `conic-gradient(#ffff ${scrollValue}%, #c0c0ff ${scrollValue}%)`;
+    }
+
+    window.addEventListener("scroll", scrollPercentage);
 });
 
 onUnmounted(() => {
-  window.removeEventListener('scroll', updateScrollPercentage);
+  window.removeEventListener("scroll", scrollPercentage);
 });
 </script>
